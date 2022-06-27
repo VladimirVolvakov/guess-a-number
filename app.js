62ef141d4b850e2f5b6f38cc2e2ee77b658e4d3e
VladimirVolvakov <dr.volvakov@gmail.com>
2022-06-27 09:58:40 +0300

    feat: add basic game values & get access to DOM elements (app.js)

---
+++ app.js
@@ -0,0 +1,13 @@
+// Game basic values:
+let minimum = 1;
+let maximum = 100;
+let winningNumber = 2;
+let attemptsLeft = 5;
+
+// UI elements:
+const game = document.querySelector('#game');
+const minNumber = document.querySelector('.min-number');
+const maxNumber = document.querySelector('.max-number');
+const guessInput = document.querySelector('#guess-input');
+const submitButton = document.querySelector('#submit-button');
+const message = document.querySelector('.message');
